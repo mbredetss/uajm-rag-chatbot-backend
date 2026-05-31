fc6ec8ef8f02fbb0dfbfd1fd674d389899efc74d
mbredets <mr.mighdadjaba@gmail.com>
2026-05-31 09:03:13 +0800

    adding cors

--- src/producer/server/index.js
+++ src/producer/server/index.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import routes from '../routes/index.js';
 import errorHandler from '../middlewares/errorHandler.js';
+import cors from 'cors';
 
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
