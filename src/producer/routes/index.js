2b1314359d81bbd310ad5dba1c2db141771232f3
mbredets <mr.mighdadjaba@gmail.com>
2026-06-06 17:10:00 +0800

    update users services: only super admin can create new user

--- src/producer/routes/index.js
+++ src/producer/routes/index.js
@@ -6,6 +6,7 @@ import webhooksRouter from '../services/webhooks/routes/index.js';
 import authentications from '../services/authentications/routes/index.js';
 import users from '../services/users/routes/index.js';
 import authenticateToken from '../middlewares/auth.js';
+import verifySuperAdmin from '../middlewares/verify-admin.js';
 
 const router = Router();
 
@@ -48,6 +49,6 @@ router.delete('/documents', authenticateToken, async (req, res, next) => {
 });
 
 router.use('/authentications', authentications);
-router.use('/users', users);
+router.use('/users', authenticateToken, verifySuperAdmin, users);
 
 export default router;
